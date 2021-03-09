b618ccacb8d61a3b86cc5457bb01c575cf54ea93
ayush <ayushthakkar28954@gmail.com>
2021-03-09 17:23:17 +0530

    Initial commit

---
+++ Practice/employee.py
@@ -0,0 +1,24 @@
+class employee:
+    id=0;
+    name=""
+    department=""
+    address=""
+    salary=0;
+
+employee_detail=[]
+no=int(input("How many employee details you want to enter?"))
+for i in range(no):
+    e1=employee()
+    e1.id=int(input("Enter id="))
+    e1.name=input("Enter name=")
+    e1.department=input("Enter your department=")
+    e1.address=input("Enter address=")
+    e1.salary=int(input("Enter salary="))
+    employee_detail.append(e1)
+
+for e1 in employee_detail:
+    print("id=",e1.id)
+    print("Name=",e1.name)
+    print("Department=",e1.department)
+    print("address=",e1.address)
+    print("salary=",e1.salary)
